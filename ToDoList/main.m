c9b5ebe8e711fd302daa2fb98c84beefa6be9e7f
Janac Meena <jm@Janacs-Mac-mini.local>
2014-02-10 09:31:41 -0500

    Initial Commit

---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Janac Meena on 2/10/2014.
+//  Copyright (c) 2014 Janac Meena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
